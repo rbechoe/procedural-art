6393ee7a355e2ff4a92f1ca45cbc3083a5a75101
Ravi Bechoe <35457924+rbechoe@users.noreply.github.com>
2022-03-14 11:07:10 +0100

    added noise

--- Assets/Scripts/ShapeGenerator.cs
+++ Assets/Scripts/ShapeGenerator.cs
@@ -5,14 +5,42 @@ using UnityEngine;
 public class ShapeGenerator
 {
     private ShapeSettings settings;
+    private NoiseFilter[] noiseFilters;
 
     public ShapeGenerator(ShapeSettings settings)
     {
         this.settings = settings;
+
+        noiseFilters = new NoiseFilter[settings.noiseLayers.Length];
+        for (int i = 0; i < noiseFilters.Length; i++)
+        {
+            noiseFilters[i] = new NoiseFilter(settings.noiseLayers[i].noiseSettings);
+        }
     }
 
     public Vector3 CalculatePointOnPlanet(Vector3 pointOnSphere)
     {
-        return pointOnSphere * settings.planetRadius;
+        float firstLayerValue = 0;
+        float elevation = 0;
+
+        if (noiseFilters.Length > 0)
+        {
+            firstLayerValue = noiseFilters[0].Evaluate(pointOnSphere);
+            if (settings.noiseLayers[0].enabled)
+            {
+                elevation = firstLayerValue;
+            }
+        }
+
+        for (int i = 1; i < noiseFilters.Length; i++)
+        {
+            if (settings.noiseLayers[i].enabled)
+            {
+                float mask = (settings.noiseLayers[i].useFirstLayerMask) ? firstLayerValue : 1;
+                elevation += noiseFilters[i].Evaluate(pointOnSphere) * mask;
+            }
+        }
+
+        return pointOnSphere * settings.planetRadius * (1 + elevation);
     }
 }
